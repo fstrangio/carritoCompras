731ac6cb8c9272238b6c8895ad29205b03a8f453
fstrangio <fedestrangio@hotmail.com>
2023-06-23 15:31:08 -0300

    https://github.com/fstrangio/carritoCompras.git

--- main.js
+++ main.js
@@ -1,19 +1,7 @@
-//Creo la clase producto para todo lo que estará en la tienda. Cada producto tendrá su id, nombre, precio e imagen
-
-class Producto {
-  constructor(id, nombre, precio, img) {
-    this.id = id;
-    this.nombre = nombre;
-    this.precio = precio;
-    this.img = img;
-    this.cantidad = 1;
-  }
-}
-
 //Asocio cada producto a su imagen correspondiente
 
 const listado = document.getElementById("listado");
-const listadoProductos = "json/productos.json";
+const listadoProductos = "./productos.json";
 
 fetch(listadoProductos)
     .then(respuesta => respuesta.json())
@@ -24,37 +12,11 @@ fetch(listadoProductos)
                                     <h2>Nombre: ${producto.nombre}</
                                     h2>
                                     <p>Precio: ${producto.precio}</p>
-                                    <p>img: ${producto.img}</p>
-
+                                    <img src="${producto.img}" alt="">
                                   `
         })
     })
     .catch(error => console.log(error))
-
-/*
-const goku = new Producto (1, "Goku", 15000, "multimedia/goku.jpg");
-const gokussj2 = new Producto (2, "Goku SSJ2", 18000, "multimedia/goku-ssj2.jpg");
-const gokussj3 = new Producto (3, "Goku SSJ3", 25000, "multimedia/goku-ssj3.jpg");
-const vegeta = new Producto (4, "Vegeta", 14000, "multimedia/vegeta.jpg");
-const vegetagod2 = new Producto (5, "Vegeta God", 27000, "multimedia/vegeta-god2.jpg");
-const gohan = new Producto (6, "Gohan", 13000, "multimedia/gohan.jpg");
-const gotenks = new Producto (7, "Gotenks", 20000, "multimedia/gotenks.jpg");
-const esferas = new Producto (8, "Esferas", 12000, "multimedia/esferas.jpg");
-const ps4dbfighterz = new Producto (9, "DB Fighter Z", 8000, "multimedia/db-fighterz.jpg");
-const ps4dbkakarot = new Producto (10, "DB Kakarot", 10000, "multimedia/db-kakarot.jpg");
-const manga1 = new Producto (11, "Manga 1", 2500, "multimedia/dbz-manga1.jpg");
-const manga7 = new Producto (12, "Manga 7", 3000, "multimedia/dbz-manga7.jpg");
-const pelicula2 = new Producto (13, "Pelicula 2", 1800, "multimedia/pelicula-2.jpg");
-const pelicula12 = new Producto (14, "Pelicula 12", 1600, "multimedia/pelicula-12.jpg");
-*/
-
-//Genero el array con todos los productos que tenemos para vender
-
-const productos = [goku, gokussj2, gokussj3, vegeta, vegetagod2, gohan, gotenks, esferas, ps4dbfighterz, ps4dbkakarot, manga1, manga7, pelicula2, pelicula12];
-
-//Creo el array que contiene el carrito
-
-let carrito = [];
 
 //Cargo el carrito desde el LocalStorage
 
